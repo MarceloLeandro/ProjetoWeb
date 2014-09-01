0793e6a12621f657f638cb851c8fd4f96577bd31
MarceloLeandro <marcelo_leandro111@hotmail.com>
2014-09-01 14:45:39 -0300

    Marcelo Teste

---
+++ ProjetoWeb/ProjetoWeb/Models/ProdutoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoWeb.Models
+{
+    public class ProdutoController : Controller
+    {
+        //
+        // GET: /Produto/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        private string descricao;
+
+        public string Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+        private float preço;
+
+        public float Preço
+        {
+            get { return preço; }
+            set { preço = value; }
+        }
+
+    }
+}
